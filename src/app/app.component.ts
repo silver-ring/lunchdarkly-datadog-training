2160e1b7371de376f6dfc38e30b98ff516ec12e8
Mohamed ElBahrawy <me@melbahrawy.com>
2022-08-24 13:38:31 +0300

    add with feature flag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as LDClient from 'launchdarkly-js-client-sdk';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'lunchdarkly-datadog-training';
+
+  featureFlag = false;
+  loading = true;
+
+  async ngOnInit() {
+    const user = {
+      key: 'user_id'
+    };
+    const client = LDClient.initialize('62ff0cb5e277b910c4a412ae', user);
+    client.waitForInitialization().then(() => {
+      this.featureFlag = client.variation('feature_1', false) as boolean;
+      this.loading = false;
+    });
+  }
+
 }
